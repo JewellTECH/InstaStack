d6704d27b89cc27676972892c56d09a3b514a077
Bradley Dice <bdice@bradleydice.com>
2015-03-08 00:38:40 -0500

    Added fonts and usernames.

--- js/instastack.js
+++ js/instastack.js
@@ -2,6 +2,7 @@ var feed = new Instafeed({
 	get: 'tagged',
 	tagName: 'jewellplc',
 	clientId: '9199226d9c454e12bb7ad4ea18ab2267',
-	resolution: 'low_resolution'
+	resolution: 'low_resolution',
+	template: '<div class="panel instaimage"><img src="{{image}}" /><p>@{{model.user.username}}</p></div>'
 });
 feed.run();
