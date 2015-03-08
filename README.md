937edf1e1d311289bec310ba9e0097b6e87bc882
Bradley Dice <bdice@bradleydice.com>
2015-03-08 01:24:38 -0500

    Added documentation, re-named project InstaStack.

---
+++ README.md
@@ -0,0 +1,12 @@
+InstaStack
+==========
+
+Designed by Bradley Dice (@Bradley_Dice) for the Interactive Digital Media course "HTML Animation" at William Jewell College.
+
+This code is based on Foundation 5, the project Instafeed.js, and jQuery.
+
+Key concepts:
+* HTML and CSS
+* Introductory JavaScript
+* APIs and JSON
+* jQuery
--- js/instastack.js
+++ js/instastack.js
@@ -1,3 +1,4 @@
+// This adds a function that lets us rotate elements smoothly, with cross-platform compatibility.
 jQuery.fn.rotate = function(degrees) {
     $(this).css({'-webkit-transform' : 'rotate('+ degrees +'deg)',
                  '-moz-transform' : 'rotate('+ degrees +'deg)',
@@ -14,20 +15,37 @@ var feed = new Instafeed({
 	template: '<div class="panel instaimage"><img src="{{image}}" /><p>@{{model.user.username}}</p></div>',
 	mock: true,
 	success: function(response){
+
+		// We need the page width and height in order to position our images.
+		var fullWidth = $( document ).width();
+		var fullHeight = $( document ).height();
+
 		for(var i = 0; i < response.data.length; i++){
-			var fullWidth = $( document ).width();
-			var fullHeight = $( document ).height();
+
+			// Now, we treat each of the response's data items separately.
 			var imageData = response.data[i];
 			var instaimage = $('<div class="panel instaimage" id="'+imageData.id+'">');
+
+			// Add the image
 			instaimage.append('<img src="'+imageData.images.low_resolution.url+'" />');
+
+			// Add the caption
 			instaimage.append('<p class="caption">'+imageData.user.full_name+'</p');
+
+			// Randomly position and rotate the image
 			instaimage.css('position', 'absolute');
 			instaimage.css('left', Math.random()*(fullWidth-350) + 'px');
 			instaimage.css('top', 100+Math.random()*(fullHeight-350-50-100) + 'px');
 			instaimage.rotate(-5 + Math.random()*10);
+
+			// Put this div into the page!
 			$('#instafeed').append(instaimage);
+
+			// Animates the image's entry
 			$('#'+imageData.id).delay(3000*i).fadeIn(1000);
+
 		}
+
 	}
 });
 feed.run();
