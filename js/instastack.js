4d0d06369205054c2a5d4c23238ea0645aa0de06
Bradley Dice <bdice@bradleydice.com>
2015-03-08 00:26:47 -0500

    Made feed images larger and removed extra content.

--- js/instastack.js
+++ js/instastack.js
@@ -1,6 +1,7 @@
 var feed = new Instafeed({
 	get: 'tagged',
 	tagName: 'jewellplc',
-	clientId: '9199226d9c454e12bb7ad4ea18ab2267'
+	clientId: '9199226d9c454e12bb7ad4ea18ab2267',
+	resolution: 'low_resolution'
 });
 feed.run();
